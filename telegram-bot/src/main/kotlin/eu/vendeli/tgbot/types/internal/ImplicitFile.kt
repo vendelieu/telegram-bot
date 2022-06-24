5ffa3ed80b7af6da7785478e91ce8fa218e0ec24
vendelieu <vendelieu@gmail.com>
2022-06-24 17:50:10 +0300

    delete redundant code

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/ImplicitFile.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/ImplicitFile.kt
@@ -4,6 +4,3 @@ sealed class ImplicitFile<T>(val file: T) {
     class FileId(fileId: String) : ImplicitFile<String>(fileId)
     class InputFile(file: ByteArray) : ImplicitFile<ByteArray>(file)
 }
-
-fun a() {
-}
