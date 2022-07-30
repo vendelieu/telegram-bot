d45c3bb648a46f8e72541f383061bcca78618a06
vendelieu <vennywise>
2022-07-30 18:01:28 +0300

    change StickerFile description

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/StickerFile.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/StickerFile.kt
@@ -8,11 +8,7 @@ package eu.vendeli.tgbot.types.internal
  *  see [animated sticker requirements](https://core.telegram.org/stickers#animated-sticker-requirements)
  *  for animated sticker technical requirements,
  *  or a WEBM video with the thumbnail up to 32 kilobytes in size;
- *  see https://core.telegram.org/stickers#video-sticker-requirements for video sticker technical requirements.
- *  Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String
- *  for Telegram to get a file from the Internet, or upload a new one using multipart/form-data.
- *  [More information on Sending Files](https://core.telegram.org/bots/api#sending-files).
- *  Animated sticker set thumbnails can't be uploaded via HTTP URL.
+ *  see [video sticker technical requirements](https://core.telegram.org/stickers#video-sticker-requirements).
  */
 sealed class StickerFile(val file: ByteArray) {
     abstract val contentType: MediaContentType
