32cebaf91fb7edd344caf8c3613111d1c1e659f7
vendelieu <vennywise>
2022-08-25 20:04:21 +0300

    add stickerType field and remove containsMasks from CreateNewStickerSet api method

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/stickerset/CreateNewStickerSet.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/stickerset/CreateNewStickerSet.kt
@@ -2,6 +2,7 @@ package eu.vendeli.tgbot.api.stickerset
 
 import eu.vendeli.tgbot.interfaces.MediaAction
 import eu.vendeli.tgbot.types.MaskPosition
+import eu.vendeli.tgbot.types.StickerType
 import eu.vendeli.tgbot.types.internal.StickerFile
 import eu.vendeli.tgbot.types.internal.TgMethod
 
@@ -10,8 +11,8 @@ class CreateNewStickerSetAction(
     title: String,
     emojis: String,
     sticker: StickerFile,
-    containsMasks: Boolean? = null,
     maskPosition: MaskPosition? = null,
+    stickerType: StickerType = StickerType.Regular,
 ) : MediaAction<Boolean> {
     override val method: TgMethod = TgMethod("createNewStickerSet")
     override val parameters: MutableMap<String, Any?> = mutableMapOf()
@@ -20,8 +21,8 @@ class CreateNewStickerSetAction(
         parameters["title"] = title
         parameters["name"] = name
         parameters["emojis"] = emojis
-        if (containsMasks != null) parameters["contains_masks"] = containsMasks
         if (maskPosition != null) parameters["mask_position"] = maskPosition
+        parameters["sticker_type"] = stickerType
 
         when (sticker) {
             is StickerFile.PNG -> setDataField("png_sticker")
@@ -38,6 +39,6 @@ fun createNewStickerSet(
     title: String,
     emojis: String,
     sticker: StickerFile,
-    containsMasks: Boolean? = null,
     maskPosition: MaskPosition? = null,
-) = CreateNewStickerSetAction(name, title, emojis, sticker, containsMasks, maskPosition)
+    stickerType: StickerType = StickerType.Regular,
+) = CreateNewStickerSetAction(name, title, emojis, sticker, maskPosition, stickerType)
