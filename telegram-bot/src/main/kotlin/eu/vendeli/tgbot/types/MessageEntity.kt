d6b858e02b3a93c645d7fc296ad66a2a9032d26c
vendelieu <vennywise>
2022-08-25 20:01:53 +0300

    add new EntityType & customEmojiId field to MessageEntity

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/MessageEntity.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/MessageEntity.kt
@@ -4,7 +4,7 @@ enum class EntityType(private val literal: String) {
     Mention("mention"), Hashtag("hashtag"), Cashtag("cashtag"), BotCommand("bot_command"),
     Url("url"), Email("email"), PhoneNumber("phone_number"), Bold("bold"), Italic("italic"),
     Underline("underline"), Strikethrough("strikethrough"), Spoiler("spoiler"), Code("code"),
-    Pre("pre"), TextLink("text_link"), TextMention("text_mention");
+    Pre("pre"), TextLink("text_link"), TextMention("text_mention"), CustomEmoji("custom_emoji");
 
     override fun toString(): String = literal
 }
@@ -16,4 +16,5 @@ data class MessageEntity(
     val url: String? = null,
     val user: User? = null,
     val language: String? = null,
+    val customEmojiId: String? = null,
 )
