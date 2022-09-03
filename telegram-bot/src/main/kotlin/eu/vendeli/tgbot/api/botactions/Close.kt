914eb595a393b79321134de8db0e7228af16cd37
vendelieu <vennywise>
2022-09-03 04:03:08 +0300

    add documentation to close method

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/botactions/Close.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/botactions/Close.kt
@@ -3,9 +3,19 @@ package eu.vendeli.tgbot.api.botactions
 import eu.vendeli.tgbot.interfaces.SimpleAction
 import eu.vendeli.tgbot.types.internal.TgMethod
 
+/**
+ * Use this method to close the bot instance before moving it from one local server to another. You need to delete the webhook before calling this method to ensure that the bot isn't launched again after server restart.
+ * The method will return error 429 in the first 10 minutes after the bot is launched. Returns True on success.
+ *
+ */
 class CloseAction : SimpleAction<Boolean> {
     override val method: TgMethod = TgMethod("close")
     override val parameters: MutableMap<String, Any?> = mutableMapOf()
 }
 
+/**
+ * Use this method to close the bot instance before moving it from one local server to another. You need to delete the webhook before calling this method to ensure that the bot isn't launched again after server restart.
+ * The method will return error 429 in the first 10 minutes after the bot is launched. Returns True on success.
+ *
+ */
 fun close() = CloseAction()
