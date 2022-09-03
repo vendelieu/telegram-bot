c5092a8370ac0a3b74809e6a4439b74e852d2ebd
vendelieu <vennywise>
2022-09-03 04:14:01 +0300

    add documentation to getMe method

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/botactions/GetMe.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/botactions/GetMe.kt
@@ -4,9 +4,17 @@ import eu.vendeli.tgbot.interfaces.SimpleAction
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.TgMethod
 
+/**
+ * A simple method for testing your bot's authentication token. Requires no parameters. Returns basic information about the bot in form of a [User](User) object.
+ *
+ */
 class GetMeAction : SimpleAction<User> {
     override val method: TgMethod = TgMethod("getMe")
     override val parameters: MutableMap<String, Any?> = mutableMapOf()
 }
 
+/**
+ * A simple method for testing your bot's authentication token. Requires no parameters. Returns basic information about the bot in form of a [User](User) object.
+ *
+ */
 fun getMe() = GetMeAction()
