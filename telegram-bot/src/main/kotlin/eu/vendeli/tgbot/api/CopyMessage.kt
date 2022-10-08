97b08eb9e57a5961b1927b0e05777033a8674877
vendelieu <vennywise>
2022-10-08 20:49:29 +0300

    fix CopyMessage, ForwardMessage methods -> add required chatId parameter

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/CopyMessage.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/CopyMessage.kt
@@ -3,10 +3,24 @@ package eu.vendeli.tgbot.api
 import eu.vendeli.tgbot.interfaces.Action
 import eu.vendeli.tgbot.interfaces.features.*
 import eu.vendeli.tgbot.types.MessageId
+import eu.vendeli.tgbot.types.internal.Recipient
 import eu.vendeli.tgbot.types.internal.TgMethod
 import eu.vendeli.tgbot.types.internal.options.CopyMessageOptions
 
-class CopyMessageAction :
+/**
+ * Use this method to copy messages of any kind. Service messages and invoice messages can't be copied.
+ * A quiz poll can be copied only if the value of the field correct_option_id is known to the bot.
+ * The method is analogous to the method forwardMessage, but the copied message doesn't have a link to the original message.
+ *
+ * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+ * @param fromChatId Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
+ * @param messageId Message identifier in the chat specified in fromChatId
+ */
+class CopyMessageAction(
+    chatId: Recipient,
+    fromChatId: Recipient,
+    messageId: Long,
+) :
     Action<MessageId>,
     OptionAble,
     MarkupAble,
@@ -18,16 +32,33 @@ class CopyMessageAction :
     override val parameters: MutableMap<String, Any?> = mutableMapOf()
     override var options = CopyMessageOptions()
 
-    constructor(fromChatId: Int, messageId: Long) {
-        parameters["from_chat_id"] = fromChatId
-        parameters["message_id"] = messageId
-    }
-
-    constructor(fromChatId: String, messageId: Long) {
-        parameters["from_chat_id"] = fromChatId
+    init {
+        parameters["chat_id"] = chatId.get()
+        parameters["from_chat_id"] = fromChatId.get()
         parameters["message_id"] = messageId
     }
 }
 
-fun copyMessage(fromChatId: Int, messageId: Long) = CopyMessageAction(fromChatId, messageId)
-fun copyMessage(fromChatId: String, messageId: Long) = CopyMessageAction(fromChatId, messageId)
+/**
+ * Use this method to copy messages of any kind. Service messages and invoice messages can't be copied.
+ * A quiz poll can be copied only if the value of the field correct_option_id is known to the bot.
+ * The method is analogous to the method forwardMessage, but the copied message doesn't have a link to the original message.
+ *
+ * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+ * @param fromChatId Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
+ * @param messageId Message identifier in the chat specified in fromChatId
+ */
+fun copyMessage(chatId: Long, fromChatId: Long, messageId: Long) =
+    CopyMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
+
+
+fun copyMessage(chatId: String, fromChatId: Long, messageId: Long) =
+    CopyMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
+
+
+fun copyMessage(chatId: Long, fromChatId: String, messageId: Long) =
+    CopyMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
+
+
+fun copyMessage(chatId: String, fromChatId: String, messageId: Long) =
+    CopyMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/ForwardMessage.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/ForwardMessage.kt
@@ -4,10 +4,18 @@ import eu.vendeli.tgbot.interfaces.Action
 import eu.vendeli.tgbot.interfaces.features.OptionAble
 import eu.vendeli.tgbot.interfaces.features.OptionsFeature
 import eu.vendeli.tgbot.types.Message
+import eu.vendeli.tgbot.types.internal.Recipient
 import eu.vendeli.tgbot.types.internal.TgMethod
 import eu.vendeli.tgbot.types.internal.options.ForwardMessageOptions
 
-class ForwardMessageAction(fromChatId: Int, messageId: Long) :
+/**
+ * Use this action to forward messages of any kind. Service messages can't be forwarded.
+ *
+ * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+ * @param fromChatId Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
+ * @param messageId Message identifier in the chat specified in fromChatId
+ */
+class ForwardMessageAction(chatId: Recipient, fromChatId: Recipient, messageId: Long) :
     Action<Message>,
     OptionAble,
     OptionsFeature<ForwardMessageAction, ForwardMessageOptions> {
@@ -16,9 +24,28 @@ class ForwardMessageAction(fromChatId: Int, messageId: Long) :
     override val parameters: MutableMap<String, Any?> = mutableMapOf()
 
     init {
-        parameters["from_chat_id"] = fromChatId
+        parameters["chat_id"] = chatId.get()
+        parameters["from_chat_id"] = fromChatId.get()
         parameters["message_id"] = messageId
     }
 }
 
-fun forwardMessage(fromChatId: Int, messageId: Long) = ForwardMessageAction(fromChatId, messageId)
+/**
+ * Use this method to forward messages of any kind. Service messages can't be forwarded.
+ *
+ * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+ * @param fromChatId Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
+ * @param messageId Message identifier in the chat specified in fromChatId
+ */
+fun forwardMessage(chatId: Long, fromChatId: Long, messageId: Long) =
+    ForwardMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
+
+
+fun forwardMessage(chatId: String, fromChatId: Long, messageId: Long) =
+    ForwardMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
+
+fun forwardMessage(chatId: Long, fromChatId: String, messageId: Long) =
+    ForwardMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
+
+fun forwardMessage(chatId: String, fromChatId: String, messageId: Long) =
+    ForwardMessageAction(Recipient.from(chatId), Recipient.from(fromChatId), messageId)
