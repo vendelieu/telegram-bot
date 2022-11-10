89141ae486dba0bee5d32a60782185dcb93f7ce8
vendelieu <vendelieu@gmail.com>
2022-11-10 17:17:40 +0300

    Revert "prevent creation empty map everytime"
    This reverts commit 21ab47a848df451f561284cad83ee9782ca1d41a.

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/Invocation.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/Invocation.kt
@@ -1,6 +1,5 @@
 package eu.vendeli.tgbot.types.internal
 
-import eu.vendeli.tgbot.utils.EMPTY_MAP
 import java.lang.reflect.Method
 
-data class Invocation(val clazz: Class<*>, val method: Method, val namedParameters: Map<String, String> = EMPTY_MAP)
+data class Invocation(val clazz: Class<*>, val method: Method, val namedParameters: Map<String, String> = mapOf())
