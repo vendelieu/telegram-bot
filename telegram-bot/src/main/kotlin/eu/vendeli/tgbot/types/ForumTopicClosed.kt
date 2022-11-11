a4536cff8a2102165ca6a763ad29e448058e5486
vendelieu <vendelieu@gmail.com>
2022-11-11 18:49:33 +0300

    add kdoc to new or changed classes

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/ForumTopicClosed.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/ForumTopicClosed.kt
@@ -1,3 +1,6 @@
 package eu.vendeli.tgbot.types
 
+/**
+ * This object represents a service message about a forum topic closed in the chat. Currently, holds no information.
+ */
 object ForumTopicClosed
