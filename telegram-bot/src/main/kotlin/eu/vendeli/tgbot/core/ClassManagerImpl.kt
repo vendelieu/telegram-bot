4638d29d3d1257eeffcc8025574e1ab712942ef3
vendelieu <vendelieu@gmail.com>
2022-11-12 22:21:33 +0300

    fix class initialization bug

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/core/ClassManagerImpl.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/core/ClassManagerImpl.kt
@@ -20,8 +20,10 @@ class ClassManagerImpl : ClassManager {
      * @return class
      */
     override fun getInstance(clazz: Class<*>, vararg initParams: Any?): Any = instances.getOrElse(clazz.name) {
-        instances[clazz.name] = if (initParams.isEmpty()) clazz.declaredConstructors.first().newInstance()
-        else clazz.declaredConstructors.first().newInstance(initParams)
+        if (initParams.isEmpty()) clazz.declaredConstructors.first().newInstance()
+        else {
+            clazz.declaredConstructors.first().newInstance(initParams)
+        }.also { instances[clazz.name] = it }
     }
 
 }
