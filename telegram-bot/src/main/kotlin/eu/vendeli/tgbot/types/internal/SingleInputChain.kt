4f1af0a769763ea4131eaf2f638d64de18280e9d
vendelieu <vendelieu@gmail.com>
2022-11-20 22:44:47 +0300

    Make appropriate changes to the internal types for the limiters to work correctly.

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/SingleInputChain.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/internal/SingleInputChain.kt
@@ -3,6 +3,7 @@ package eu.vendeli.tgbot.types.internal
 class SingleInputChain(
     internal val id: String,
     internal val inputAction: suspend InputContext.() -> Unit,
+    internal val rateLimits: RateLimits,
     internal val currentLevel: Int = 0,
     internal var tail: String? = null,
     internal var breakPoint: InputBreakPoint? = null,
