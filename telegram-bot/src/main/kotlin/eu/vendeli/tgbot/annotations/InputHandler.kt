31af5815085b60a06d3c6281476e00d58f0651e7
vendelieu <vendelieu@gmail.com>
2022-11-20 22:40:43 +0300

    Add new RateLimits annotation

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/annotations/InputHandler.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/annotations/InputHandler.kt
@@ -4,9 +4,11 @@ package eu.vendeli.tgbot.annotations
  * The annotation used to denote the function that is used to process the specified I/O event.
  *
  * @property value Keywords of the input.
+ * @property rateLimits Query limits for this particular command.
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class InputHandler(
-    val value: Array<String>
+    val value: Array<String>,
+    val rateLimits: RateLimits = RateLimits(0, 0)
 )
