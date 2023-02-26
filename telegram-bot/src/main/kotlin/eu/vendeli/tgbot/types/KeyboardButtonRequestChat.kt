f175d64df07bcfde7c5d828027ccc0a958b37cd6
vendelieu <vendelieu@gmail.com>
2023-02-26 15:18:20 +0100

    add keyboardbutton requestChat and requestUser fields

---
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/KeyboardButtonRequestChat.kt
@@ -0,0 +1,12 @@
+package eu.vendeli.tgbot.types
+
+data class KeyboardButtonRequestChat(
+    val requestId: Int,
+    val chatIsChannel: Boolean,
+    val chatIsForum: Boolean? = null,
+    val chatHasUserName: Boolean? = null,
+    val chatIsCreated: Boolean? = null,
+    val userAdministratorRights: ChatAdministratorRights? = null,
+    val botAdministratorRights: ChatAdministratorRights? = null,
+    val botIsMember: Boolean? = null
+)
