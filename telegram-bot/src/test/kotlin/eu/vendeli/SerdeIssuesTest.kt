24de695d3ded4736fb29ce45bdb6ac111dba0d07
vendelieu <vendelieu@gmail.com>
2023-04-18 20:42:02 +0300

    add serialization test for markup parameter

--- telegram-bot/src/test/kotlin/eu/vendeli/SerdeIssuesTest.kt
+++ telegram-bot/src/test/kotlin/eu/vendeli/SerdeIssuesTest.kt
@@ -2,13 +2,16 @@ package eu.vendeli
 
 import BotTestContext
 import eu.vendeli.tgbot.TelegramBot.Companion.mapper
+import eu.vendeli.tgbot.types.keyboard.KeyboardButton
+import eu.vendeli.tgbot.types.keyboard.ReplyKeyboardMarkup
 import eu.vendeli.tgbot.types.media.Voice
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldContain
 
-class SerializationIssuesTest : BotTestContext() {
+class SerdeIssuesTest : BotTestContext() {
     @Test
-    fun `test Voice type for serialization`() {
+    fun `Voice type deserialization`() {
         val testData = """{"file_id": "test", "file_unique_id": "test1", "duration": 1, "mime_type": "audio/ogg"}"""
         val voice = mapper.runCatching { readValue(testData, Voice::class.java) }
 
@@ -22,5 +25,12 @@ class SerializationIssuesTest : BotTestContext() {
             mimeType shouldNotBe null
             mimeType shouldBe "audio/ogg"
         }
+    }
+
+    @Test
+    fun `reply markup parameter wrong serialization`() {
+        val markup = ReplyKeyboardMarkup(listOf(listOf(KeyboardButton("test"))), inputFieldPlaceholder = "test")
+        val serializedMarkup = mapper.writeValueAsString(markup)
+        serializedMarkup shouldContain "input_field_placeholder"
     }
 }
