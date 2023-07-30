638979e97763068bcd01b14883e568bf0ca5917d
vendelieu <vendelieu@gmail.com>
2023-07-31 00:14:07 +0300

    fix getGameHighScores method

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/game/GameHighScore.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/types/game/GameHighScore.kt
@@ -1,9 +1,10 @@
 package eu.vendeli.tgbot.types.game
 
+import eu.vendeli.tgbot.interfaces.MultipleResponse
 import eu.vendeli.tgbot.types.User
 
 data class GameHighScore(
     val position: Int,
     val user: User,
-    val score: Int,
-)
+    val score: Long,
+) : MultipleResponse
