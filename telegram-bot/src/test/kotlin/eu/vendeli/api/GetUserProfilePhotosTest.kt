ebc3e465c7591bcc0dfc9bde38217f9e83d37ca1
vendelieu <vendelieu@gmail.com>
2023-07-30 22:44:46 +0300

    add more method test

--- telegram-bot/src/test/kotlin/eu/vendeli/api/GetUserProfilePhotosTest.kt
+++ telegram-bot/src/test/kotlin/eu/vendeli/api/GetUserProfilePhotosTest.kt
@@ -1,31 +1,28 @@
 package eu.vendeli.api
 
 import BotTestContext
-import eu.vendeli.tgbot.api.dice
+import eu.vendeli.tgbot.api.getUserProfilePhotos
 import eu.vendeli.tgbot.types.internal.getOrNull
 import eu.vendeli.tgbot.types.internal.isSuccess
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 
-class DiceTest : BotTestContext() {
+class GetUserProfilePhotosTest : BotTestContext() {
     @Test
-    suspend fun `location method test`() {
-        val request = dice(emoji = "\uD83C\uDFC0").options {
-            protectContent = true
-        }.sendAsync(TG_ID, bot).await()
+    suspend fun `get user profile photos method test`() {
+        val request = getUserProfilePhotos(TG_ID, 0, 1).sendAsync(bot).await()
 
         val result = with(request) {
             ok.shouldBeTrue()
             isSuccess().shouldBeTrue()
             getOrNull().shouldNotBeNull()
         }
-        result.hasProtectedContent shouldBe true
 
-        with(result.dice) {
+        with(result) {
             shouldNotBeNull()
-            emoji shouldBe "\uD83C\uDFC0"
-            value.shouldNotBeNull()
+            totalCount shouldBe 1
+            photos.shouldNotBeNull()
         }
     }
 }
