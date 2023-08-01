820f17597b2c53f4bd98510a09ef666e87a36c4f
vendelieu <vendelieu@gmail.com>
2023-08-01 10:06:17 +0300

    delete message tail

--- telegram-bot/src/test/kotlin/BotTestContext.kt
+++ telegram-bot/src/test/kotlin/BotTestContext.kt
@@ -1,8 +1,6 @@
-
 import ch.qos.logback.classic.Level.TRACE
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.interfaces.Action
-import eu.vendeli.tgbot.types.Message
 import eu.vendeli.tgbot.types.internal.HttpLogLevel
 import eu.vendeli.tgbot.types.internal.Response
 import eu.vendeli.utils.MockUpdate
@@ -17,17 +15,13 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
 import io.ktor.utils.io.ByteReadChannel
-import kotlinx.coroutines.delay
 import java.time.Instant
-import kotlin.time.Duration.Companion.milliseconds
 
 @Suppress("VariableNaming", "PropertyName", "PrivatePropertyName")
 abstract class BotTestContext(
     private val withPreparedBot: Boolean = true,
     private val mockHttp: Boolean = false,
 ) : AnnotationSpec() {
-    private val messageTail: MutableList<Long> = mutableListOf()
-
     protected lateinit var bot: TelegramBot
     protected var classloader: ClassLoader = Thread.currentThread().contextClassLoader
 
@@ -67,18 +61,8 @@ abstract class BotTestContext(
         )
     }
 
-    @Suppress("UNCHECKED_CAST")
-    protected suspend fun <T> Action<T>.sendReturning(id: Long, bot: TelegramBot): Response<out T> {
-        val response = sendAsync(id, bot).await()
-
-        response.runCatching {
-            (this as? Response.Success<Message>)?.result?.messageId?.also {
-                messageTail.add(it)
-            }
-        }
-        delay(200.milliseconds)
-        return response
-    }
+    protected suspend fun <T> Action<T>.sendReturning(id: Long, bot: TelegramBot): Response<out T> =
+        sendAsync(id, bot).await()
 
     protected suspend fun getExtFile(url: String): ByteArray = bot.httpClient.get(url).readBytes()
 }
