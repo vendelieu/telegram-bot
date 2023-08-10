e84df6e9cf656e04d8542a8ac0b0590195a75bac
vendelieu <vendelieu@gmail.com>
2023-08-10 04:39:53 +0300

    add input listener test

---
+++ telegram-bot/src/test/kotlin/eu/vendeli/InputListenerTest.kt
@@ -0,0 +1,23 @@
+package eu.vendeli
+
+import BotTestContext
+import eu.vendeli.tgbot.core.InputListenerMapImpl
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+
+class InputListenerTest : BotTestContext() {
+    private val mapImpl = InputListenerMapImpl()
+    @Test
+    fun `sync crud test`() {
+        mapImpl.set(1, "test")
+        mapImpl.get(1) shouldBe "test"
+        mapImpl.del(1)
+    }
+
+    @Test
+    suspend fun `async crud test`() {
+        mapImpl.setAsync(1, "test").await().shouldBeTrue()
+        mapImpl.getAsync(1).await() shouldBe "test"
+        mapImpl.delAsync(1).await().shouldBeTrue()
+    }
+}
