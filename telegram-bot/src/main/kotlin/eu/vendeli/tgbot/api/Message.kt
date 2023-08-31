99788e72782f7a4c8622f340b41061bd2167787d
vendelieu <vendelieu@gmail.com>
2023-08-31 06:19:26 +0300

    improve interfaces

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/Message.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/api/Message.kt
@@ -5,7 +5,8 @@ package eu.vendeli.tgbot.api
 import eu.vendeli.tgbot.interfaces.Action
 import eu.vendeli.tgbot.interfaces.ActionState
 import eu.vendeli.tgbot.interfaces.TgAction
-import eu.vendeli.tgbot.interfaces.features.AllFeaturesPack
+import eu.vendeli.tgbot.interfaces.features.EntitiesFeature
+import eu.vendeli.tgbot.interfaces.features.MarkupFeature
 import eu.vendeli.tgbot.interfaces.features.OptionsFeature
 import eu.vendeli.tgbot.types.Message
 import eu.vendeli.tgbot.types.internal.TgMethod
@@ -14,7 +15,12 @@ import eu.vendeli.tgbot.utils.builders.EntitiesContextBuilder
 import eu.vendeli.tgbot.utils.getReturnType
 
 class SendMessageAction private constructor() :
-    Action<Message>, ActionState(), AllFeaturesPack<SendMessageAction, MessageOptions>, EntitiesContextBuilder {
+    Action<Message>,
+    ActionState(),
+    OptionsFeature<SendMessageAction, MessageOptions>,
+    MarkupFeature<SendMessageAction>,
+    EntitiesFeature<SendMessageAction>,
+    EntitiesContextBuilder {
     override val TgAction<Message>.method: TgMethod
         get() = TgMethod("sendMessage")
     override val TgAction<Message>.returnType: Class<Message>
