cbb59d96003bb49c9fe0528e19b9abff0bc8c418
vendelieu <vendelieu@gmail.com>
2023-10-27 04:52:48 +0300

    add shortcuts for InputListener

--- telegram-bot/src/main/kotlin/eu/vendeli/tgbot/interfaces/InputListener.kt
+++ telegram-bot/src/main/kotlin/eu/vendeli/tgbot/interfaces/InputListener.kt
@@ -1,5 +1,6 @@
 package eu.vendeli.tgbot.interfaces
 
+import eu.vendeli.tgbot.types.User
 import kotlinx.coroutines.Deferred
 
 /**
@@ -17,6 +18,14 @@ interface InputListener {
     fun set(telegramId: Long, identifier: String)
 
     /**
+     * Shortcut function for [set] method.
+     *
+     * @param user
+     * @param identifier
+     */
+    fun set(user: User, identifier: () -> String): Unit = set(user.id, identifier())
+
+    /**
      * Asynchronously set new waiting point
      *
      * @param telegramId
@@ -25,11 +34,29 @@ interface InputListener {
     suspend fun setAsync(telegramId: Long, identifier: String): Deferred<Boolean>
 
     /**
+     * Shortcut function for [setAsync] method.
+     *
+     * @param user
+     * @param identifier
+     * @return [Deferred]<[Boolean]>
+     */
+    suspend fun setAsync(user: User, identifier: () -> String): Deferred<Boolean> =
+        setAsync(user.id, identifier())
+
+    /**
      * Get current waiting input of user.
      *
      * @param telegramId
      */
     fun get(telegramId: Long): String?
+
+    /**
+     * Get operator shortcut function for getting value.
+     *
+     * @see get
+     * @param user
+     */
+    operator fun get(user: User): String? = get(user.id)
 
     /**
      * Asynchronously get current waiting input of user.
--- telegram-bot/src/test/kotlin/eu/vendeli/InputListenerTest.kt
+++ telegram-bot/src/test/kotlin/eu/vendeli/InputListenerTest.kt
@@ -2,7 +2,9 @@ package eu.vendeli
 
 import BotTestContext
 import eu.vendeli.tgbot.core.InputListenerMapImpl
+import eu.vendeli.tgbot.types.User
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 
 class InputListenerTest : BotTestContext() {
@@ -13,6 +15,11 @@ class InputListenerTest : BotTestContext() {
         mapImpl.set(1, "test")
         mapImpl.get(1) shouldBe "test"
         mapImpl.del(1)
+        mapImpl.get(1).shouldBeNull()
+
+        val user = User(1, false, "Test")
+        mapImpl.set(user) { "test2" }
+        mapImpl[user] shouldBe "test2"
     }
 
     @Test
@@ -20,5 +27,10 @@ class InputListenerTest : BotTestContext() {
         mapImpl.setAsync(1, "test").await().shouldBeTrue()
         mapImpl.getAsync(1).await() shouldBe "test"
         mapImpl.delAsync(1).await().shouldBeTrue()
+        mapImpl.getAsync(1).shouldBeNull()
+
+        val user = User(1, false, "Test")
+        mapImpl.setAsync(user) { "test2" }.await().shouldBeTrue()
+        mapImpl.getAsync(user.id) shouldBe "test2"
     }
 }
