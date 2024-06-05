2ce57d2a0ba0aa19fae63acb0fc74a7c80e18e48
vendelieu <vendelieu@gmail.com>
2024-06-05 23:41:36 +0300

    add test for refundstarpayment

--- telegram-bot/src/jvmTest/kotlin/ProjectConfig.kt
+++ telegram-bot/src/jvmTest/kotlin/ProjectConfig.kt
@@ -1,9 +1,11 @@
 @file:Suppress("unused")
 
 import io.kotest.common.ExperimentalKotest
+import io.kotest.core.annotation.AutoScan
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.test.TestCaseOrder
 
+@AutoScan
 object ProjectConfig : AbstractProjectConfig() {
     @ExperimentalKotest
     override val concurrentTests: Int = 1
