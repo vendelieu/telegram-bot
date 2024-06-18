b974dff06237bea089dfa0b601d6b7c4f4fbcf4b
vendelieu <vendelieu@gmail.com>
2024-06-18 14:38:49 +0300

    add test for getStarTransactions

---
+++ telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/botactions/GetStarTransactionsTest.kt
@@ -0,0 +1,16 @@
+package eu.vendeli.api.botactions
+
+import BotTestContext
+import eu.vendeli.tgbot.api.botactions.getStarTransactions
+import io.kotest.matchers.shouldBe
+
+class GetStarTransactionsTest : BotTestContext() {
+    @Test
+    suspend fun `getStarTransactions method testing`() {
+        val result = getStarTransactions(10, 20).sendAsync(bot).shouldSuccess()
+
+        result.run {
+            transactions.size shouldBe 0
+        }
+    }
+}
