9c91d4be7534589842d1309b72655e1bc5f9da05
vendelieu <vendelieu@gmail.com>
2024-06-22 17:16:07 +0300

    add collection ext for building inlineKeyboardMarkup

--- telegram-bot/src/commonMain/kotlin/eu/vendeli/tgbot/utils/CommonExtenstions.kt
+++ telegram-bot/src/commonMain/kotlin/eu/vendeli/tgbot/utils/CommonExtenstions.kt
@@ -1,5 +1,7 @@
 package eu.vendeli.tgbot.utils
 
+import eu.vendeli.tgbot.types.keyboard.InlineKeyboardMarkup
+import eu.vendeli.tgbot.utils.builders.inlineKeyboardMarkup
 import io.ktor.http.decodeURLQueryComponent
 import korlibs.crypto.HMAC
 
@@ -12,4 +14,22 @@ fun String.checkIsInitDataSafe(botToken: String, hash: String): Boolean {
         .joinToString("\n")
 
     return HMAC.hmacSHA256(secretKey.bytes, decodedData.encodeToByteArray()).hexLower == hash.lowercase()
+}
+
+
+fun <T> Collection<T>.joinToInlineKeyboard(
+    nameSelector: T.() -> String,
+    callbackSelector: T.() -> String,
+    currentPage: Int = 1,
+    perPage: Int = 5,
+    perLine: Int = 1,
+): InlineKeyboardMarkup = inlineKeyboardMarkup {
+    val elWindowLeftEdge = if (currentPage == 1) 0 else ((currentPage - 1) * perPage - 1)
+    val elWindowRightEdge = if (currentPage == 1) perPage else (currentPage * perPage - 1)
+    val pageElWindow = toList().subList(elWindowLeftEdge, elWindowRightEdge.takeIf { it <= size } ?: size)
+
+    pageElWindow.forEachIndexed { idx, i ->
+        callbackData(i.nameSelector()) { i.callbackSelector() }
+        if (idx == perLine - 1) br()
+    }
 }
