597cb0e6599619097ca791efce3bb42d37ca4ef2
vendelieu <vendelieu@gmail.com>
2024-07-07 16:22:50 +0300

    remove enabledIf

--- telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/media/SendPaidMediaTest.kt
+++ telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/media/SendPaidMediaTest.kt
@@ -1,17 +1,14 @@
 package eu.vendeli.api.media
 
 import BotTestContext
-import ChannelTestingOnlyCondition
 import eu.vendeli.tgbot.api.media.sendPaidMedia
 import eu.vendeli.tgbot.types.media.InputPaidMedia
 import eu.vendeli.tgbot.utils.toImplicitFile
-import io.kotest.core.annotation.EnabledIf
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import utils.RandomPicResource.RANDOM_PIC_URL
 import java.io.File
 
-@EnabledIf(ChannelTestingOnlyCondition::class)
 class SendPaidMediaTest : BotTestContext() {
     @Test
     suspend fun `sendPaidMedia action testing`() {
