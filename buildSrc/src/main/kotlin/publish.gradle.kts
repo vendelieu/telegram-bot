35ab85e98726d80bc3ee6ede4170bc52de3d209d
vendelieu <vendelieu@gmail.com>
2024-07-23 17:54:26 +0300

    add snapshot repo

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -53,7 +53,11 @@ publishing {
                 username = sonatypeUsername
                 password = sonatypePassword
             }
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = if (version.toString().contains("SNAPSHOT")) {
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            } else {
+                uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
         }
         maven {
             name = "GHPackages"
