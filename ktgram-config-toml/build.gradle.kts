7fa15062cf007572d4746bde5c1562ccdd801b89
vendelieu <vendelieu@gmail.com>
2024-08-12 02:44:03 +0300

    try build

--- ktgram-config-toml/build.gradle.kts
+++ ktgram-config-toml/build.gradle.kts
@@ -11,9 +11,7 @@ onlyJvmConfiguredKotlin {
             dependencies {
                 implementation(project(":telegram-bot"))
                 implementation(libs.kotlin.datetime)
-                implementation(libs.ktoml.core.get().let { it.group + ":" + it.name + ":" + it.version }) {
-                    exclude("org.jetbrains.kotlinx", "kotlinx-datetime")
-                }
+                implementation(libs.ktoml.core)
                 implementation(libs.ktoml.file)
             }
         }
