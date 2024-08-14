a2ee0f16f4b27976fefd7837b742b23fda7b1e1c
vendelieu <vendelieu@gmail.com>
2024-08-14 21:50:51 +0300

    improve validation

--- helper/src/jvmMain/kotlin/eu/vendeli/ksp/TypesValidator.kt
+++ helper/src/jvmMain/kotlin/eu/vendeli/ksp/TypesValidator.kt
@@ -3,13 +3,14 @@ package eu.vendeli.ksp
 import com.google.devtools.ksp.getDeclaredProperties
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
 internal fun ApiProcessor.validateTypes(classes: Sequence<KSClassDeclaration>, apiFile: JsonElement) {
-    val types = apiFile.jsonObject["types"]!!.jsonObject
+    val types = apiFile.jsonObject["types"]!!.jsonObject.toMap()
+    val visitedTypes = mutableSetOf<String>()
+    visitedTypes.add("InputFile") // in internal types
 
     classes.forEach { cls ->
         val className = cls.annotations
@@ -22,6 +23,7 @@ internal fun ApiProcessor.validateTypes(classes: Sequence<KSClassDeclaration>, a
         val sealedSubclasses = cls.getSealedSubclasses()
 
         if (sealedSubclasses.any()) {
+            visitedTypes.add(className)
             sealedSubclasses.forEach sealedLoop@{ s ->
                 val sealedName = s.simpleName.getShortName()
                 val sealedFullName = s.qualifiedName!!.asString()
@@ -35,7 +37,7 @@ internal fun ApiProcessor.validateTypes(classes: Sequence<KSClassDeclaration>, a
                     ?.toString()
                     ?: (className + sealedName)
 
-                processClass(types, sealedParams, apiName, sealedFullName)
+                processClass(types, sealedParams, apiName, sealedFullName, visitedTypes)
             }
             return@forEach
         }
@@ -43,11 +45,19 @@ internal fun ApiProcessor.validateTypes(classes: Sequence<KSClassDeclaration>, a
         val classFullName = cls.qualifiedName!!.asString()
         val classParams = cls.getDeclaredProperties().map { it.simpleName.asString() }.toSet()
 
-        processClass(types, classParams, className, classFullName)
+        processClass(types, classParams, className, classFullName, visitedTypes)
     }
+    val leftTypes = types.keys - visitedTypes
+    if (leftTypes.isNotEmpty()) logger.warn("Not all types have been processed; remaining are:: $leftTypes")
 }
 
-private fun ApiProcessor.processClass(types: JsonObject, params: Set<String>, name: String, fullName: String) {
+private fun ApiProcessor.processClass(
+    types: Map<String, JsonElement>,
+    params: Set<String>,
+    name: String,
+    fullName: String,
+    visitedTypesRef: MutableSet<String>,
+) {
     val typeInfo = types[name]?.jsonObject
     if (typeInfo == null) {
         logger.warn("Class $fullName not found specs and validation is omitted.")
@@ -66,4 +76,5 @@ private fun ApiProcessor.processClass(types: JsonObject, params: Set<String>, na
                 ),
             )
     }
+    visitedTypesRef.add(name)
 }
