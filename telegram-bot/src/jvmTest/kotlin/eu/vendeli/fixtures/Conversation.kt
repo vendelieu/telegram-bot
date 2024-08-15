0a1da5b579d3641b31f30c4cd587ca5b8f922009
vendelieu <vendelieu@gmail.com>
2024-08-15 14:23:43 +0300

    apply stateful link example

--- telegram-bot/src/jvmTest/kotlin/eu/vendeli/fixtures/Conversation.kt
+++ telegram-bot/src/jvmTest/kotlin/eu/vendeli/fixtures/Conversation.kt
@@ -3,23 +3,22 @@ package eu.vendeli.fixtures
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.InputChain
 import eu.vendeli.tgbot.api.message.message
-import eu.vendeli.tgbot.implementations.ChainStateManagerImpl
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.BreakCondition
 import eu.vendeli.tgbot.types.internal.ChainLink
 import eu.vendeli.tgbot.types.internal.ProcessedUpdate
-import eu.vendeli.tgbot.types.internal.StateSelector
+import eu.vendeli.tgbot.types.internal.chain.BaseStatefulLink
 
 @InputChain
-class Conversation : ChainStateManagerImpl() {
-    object Name : ChainLink() {
+class Conversation {
+    object Name : BaseStatefulLink() {
         override val breakCondition = BreakCondition { _, update, _ -> update.text.isEmpty() }
 
-        override suspend fun action(user: User, update: ProcessedUpdate, bot: TelegramBot) {
-            user["name"] = update.text
-
+        override suspend fun action(user: User, update: ProcessedUpdate, bot: TelegramBot): String {
             message { "Oh, ${update.text}, nice to meet you!" }.send(user, bot)
             message { "How old are you?" }.send(user, bot)
+
+            return update.text
         }
 
         override suspend fun breakAction(user: User, update: ProcessedUpdate, bot: TelegramBot) {
@@ -32,10 +31,9 @@ class Conversation : ChainStateManagerImpl() {
     object Age : ChainLink() {
         override val breakCondition = BreakCondition { _, update, _ -> update.text.toIntOrNull() == null }
         override val retryAfterBreak = false
-        override val stateSelector = StateSelector.Custom { updateId }
 
         override suspend fun action(user: User, update: ProcessedUpdate, bot: TelegramBot) {
-            val name = user["name"]
+            val name = Name.state.get(user)
             message {
                 "I'm not good at remembering, but I remembered you! You're $name and you're ${update.text} years old."
             }.send(user, bot)
