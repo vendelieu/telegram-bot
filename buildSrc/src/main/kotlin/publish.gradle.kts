a7467ff5d22920a83bc1a2ad724ee5b03f463b34
vendelieu <vendelieu@gmail.com>
2024-08-18 16:25:17 +0300

    update build script

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -12,17 +12,21 @@ val doSign = providers.gradleProperty("signing.keyId").isPresent
 val isMultiplatform = !project.plugins.hasPlugin("org.jetbrains.kotlin.jvm")
 
 if (isMultiplatform) apply(plugin = "org.jetbrains.kotlin.multiplatform")
+val isJavadocAlreadyPresent = project.plugins.hasPlugin("com.gradle.plugin-publish")
 
 mavenPublishing {
     coordinates("eu.vendeli", project.name, project.version.toString())
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, true)
     if (doSign) signAllPublications()
 
-    val javaDoc = if (project.name == "telegram-bot" && doSign) JavadocJar.Dokka("dokkaHtml")
-    else JavadocJar.Empty()
+    val javaDoc = when {
+        project.name == "telegram-bot" && doSign -> JavadocJar.Dokka("dokkaHtml")
+        isJavadocAlreadyPresent -> JavadocJar.None()
+        else -> JavadocJar.Empty()
+    }
 
     val platformArtifact = if (isMultiplatform) KotlinMultiplatform(javaDoc, true)
-    else KotlinJvm(javaDoc, true)
+    else KotlinJvm(JavadocJar.None(), true)
 
     configure(platformArtifact)
 
