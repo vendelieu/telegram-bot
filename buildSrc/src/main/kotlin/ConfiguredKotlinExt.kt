e5ca720de9da9fb2d0437efb9c4a13e0f9fd2006
vendelieu <vendelieu@gmail.com>
2024-08-22 21:26:17 +0300

    fix build

--- buildSrc/src/main/kotlin/ConfiguredKotlinExt.kt
+++ buildSrc/src/main/kotlin/ConfiguredKotlinExt.kt
@@ -1,26 +1,31 @@
-
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 private val jvmTargetVer = JvmTarget.JVM_17
 
+private fun KotlinMultiplatformExtension.configureJvm() {
+    jvm {
+        withJava()
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(jvmTargetVer)
+                    freeCompilerArgs = listOf("-Xjsr305=strict")
+                }
+            }
+        }
+    }
+    jvmToolchain(jvmTargetVer.target.toInt())
+}
+
 fun Project.onlyJvmConfiguredKotlin(block: KotlinMultiplatformExtension.() -> Unit) {
     plugins.apply("kotlin-multiplatform")
 
     configure<KotlinMultiplatformExtension> {
-        jvm {
-            withJava()
-            compilations.all {
-                compileTaskProvider.configure {
-                    compilerOptions {
-                        jvmTarget.set(jvmTargetVer)
-                    }
-                }
-            }
-        }
-        jvmToolchain(jvmTargetVer.target.toInt())
+        configureJvm()
         block()
     }
 }
@@ -29,20 +34,10 @@ fun Project.configuredKotlin(block: KotlinMultiplatformExtension.() -> Unit) {
     plugins.apply("kotlin-multiplatform")
 
     configure<KotlinMultiplatformExtension> {
-        jvm {
-            withJava()
-            compilations.all {
-                compileTaskProvider.configure {
-                    compilerOptions {
-                        jvmTarget.set(jvmTargetVer)
-                    }
-                }
-            }
-        }
+        configureJvm()
         js { nodejs() }
         mingwX64()
         linuxX64()
-        jvmToolchain(jvmTargetVer.target.toInt())
         block()
     }
 }
