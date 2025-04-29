1e4874834157329f58d13f5f2b4adf70e986c982
vendelieu <vendelieu@gmail.com>
2025-04-29 06:10:27 +0300

    improve test

--- telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/story/StoryApiTest.kt
+++ telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/story/StoryApiTest.kt
@@ -4,23 +4,29 @@ import BotTestContext
 import eu.vendeli.tgbot.api.story.deleteStory
 import eu.vendeli.tgbot.api.story.editStory
 import eu.vendeli.tgbot.api.story.postStory
+import eu.vendeli.tgbot.types.component.ImplicitFile
 import eu.vendeli.tgbot.types.story.InputStoryContent
 import eu.vendeli.tgbot.utils.common.toImplicitFile
 import io.kotest.matchers.shouldBe
 import io.ktor.client.request.get
 import io.ktor.client.statement.readRawBytes
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import utils.RandomPicResource
 import kotlin.time.Duration.Companion.seconds
 
 class StoryApiTest : BotTestContext() {
+    private val image: ImplicitFile by lazy {
+        runBlocking {
+            bot.httpClient
+                .get(RandomPicResource.current.getPicUrl(400, 400))
+                .readRawBytes()
+                .toImplicitFile("test.png", "image/png")
+        }
+    }
+
     @Test
     fun `postStory test`() = runTest {
-        val image = bot.httpClient
-            .get(RandomPicResource.current.getPicUrl(400, 400))
-            .readRawBytes()
-            .toImplicitFile("test.png", "image/png")
-
         postStory("test", InputStoryContent.Photo(image), 10.seconds)
             .sendReq()
             .shouldFailure()
@@ -30,11 +36,6 @@ class StoryApiTest : BotTestContext() {
 
     @Test
     fun `editStory test`() = runTest {
-        val image = bot.httpClient
-            .get(RandomPicResource.current.getPicUrl(400, 400))
-            .readRawBytes()
-            .toImplicitFile("test.png", "image/png")
-
         editStory("test", "story_id", InputStoryContent.Photo(image))
             .sendReq()
             .shouldFailure()
