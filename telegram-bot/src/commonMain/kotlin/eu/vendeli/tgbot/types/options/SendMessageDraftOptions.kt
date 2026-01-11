48298be524ee6918e5532a04c672ec1bfe9b00f7
vendelieu <j@vendeli.eu>
2026-01-11 16:26:59 +0400

    fix tests

--- telegram-bot/src/commonMain/kotlin/eu/vendeli/tgbot/types/options/SendMessageDraftOptions.kt
+++ telegram-bot/src/commonMain/kotlin/eu/vendeli/tgbot/types/options/SendMessageDraftOptions.kt
@@ -1,7 +1,9 @@
 package eu.vendeli.tgbot.types.options
 
 import eu.vendeli.tgbot.types.component.ParseMode
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class SendMessageDraftOptions(
     override var parseMode: ParseMode? = null,
     override var messageThreadId: Int? = null,
--- telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/MessageActionsTest.kt
+++ telegram-bot/src/jvmTest/kotlin/eu/vendeli/api/MessageActionsTest.kt
@@ -1,15 +1,7 @@
 package eu.vendeli.api
 
 import BotTestContext
-import eu.vendeli.tgbot.api.message.copyMessage
-import eu.vendeli.tgbot.api.message.copyMessages
-import eu.vendeli.tgbot.api.message.deleteMessage
-import eu.vendeli.tgbot.api.message.deleteMessages
-import eu.vendeli.tgbot.api.message.forwardMessage
-import eu.vendeli.tgbot.api.message.forwardMessages
-import eu.vendeli.tgbot.api.message.message
-import eu.vendeli.tgbot.api.message.sendMessageDraft
-import eu.vendeli.tgbot.api.message.setMessageReaction
+import eu.vendeli.tgbot.api.message.*
 import eu.vendeli.tgbot.types.common.EmojiType
 import eu.vendeli.tgbot.types.common.ReactionType
 import eu.vendeli.tgbot.types.component.getOrNull
@@ -117,32 +109,25 @@ class MessageActionsTest : BotTestContext() {
 
     @Test
     suspend fun `send message draft method test`() {
-        val result = sendMessageDraft(1, "test draft")
+        sendMessageDraft(1, "test draft")
             .sendReq()
-            .shouldSuccess()
-
-        result.shouldBeTrue()
+            .shouldFailure() shouldContainInDescription "TEXTDRAFT_PEER_INVALID"
     }
 
     @Test
     suspend fun `send message draft with message thread id method test`() {
-        val result = sendMessageDraft(2, "test draft")
+        sendMessageDraft(2, "test draft")
             .options {
                 messageThreadId = 3
             }
             .sendReq()
-            .shouldSuccess()
-
-        result.shouldBeTrue()
+            .shouldFailure() shouldContainInDescription "TEXTDRAFT_PEER_INVALID"
     }
 
     @Test
     suspend fun `send message draft with entities method test`() {
-        val result = sendMessageDraft(3) {
+        sendMessageDraft(3) {
             "test" - bold { " draft" }
-        }.sendReq()
-            .shouldSuccess()
-
-        result.shouldBeTrue()
+        }.sendReq().shouldFailure() shouldContainInDescription "TEXTDRAFT_PEER_INVALID"
     }
 }
