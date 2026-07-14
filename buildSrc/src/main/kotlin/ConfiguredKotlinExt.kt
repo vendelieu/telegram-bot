71741179ede322a224e8f3dff15c8a4de23360d0
vendelieu <j@vendeli.eu>
2026-07-14 16:56:43 +0400

    disable configuration on demand; remove deprecated compiler arg

--- buildSrc/src/main/kotlin/ConfiguredKotlinExt.kt
+++ buildSrc/src/main/kotlin/ConfiguredKotlinExt.kt
@@ -15,7 +15,6 @@ private fun KotlinCommonCompilerOptions.configureCompilerOptions() {
         "-opt-in=eu.vendeli.tgbot.annotations.internal.ExperimentalFeature",
         "-opt-in=kotlin.time.ExperimentalTime",
         "-Xwarning-level=NOTHING_TO_INLINE:disabled",
-        "-Xcontext-parameters"
     )
 }
 
